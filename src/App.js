399023d8a2b66aac87fdd8cf6efd40c806fc1d26
Roberto Arce <rarcemu@gmail.com>
2022-08-24 17:53:50 -0400

    sync

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { DiagramComponent } from "@syncfusion/ej2-react-diagrams";
+let node = [{
+  // Position of the node
+  offsetX: 250,
+  offsetY: 250,
+  // Size of the node
+  width: 100,
+  height: 100,
+  style: {
+      fill: '#6BA5D7',
+      strokeColor: 'white'
+  },
+}];
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DiagramComponent id="container" width={'100%'} height={'600px'} 
+// Add node
+      nodes={node}/>
     </div>
   );
 }
